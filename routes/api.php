9ce2d21a9656959c8db132ccab31107a0d72841b
ixkka <janfrancesca.logrono.20@usjr.edu.ph>
2024-07-18 13:09:39 +0800

    updated routes

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ use App\Http\Controllers\api\DirectorController;
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/actors/{id}', [ActorController::class, 'show']);
     Route::get('/director/{id}', [DirectorController::class, 'show']);
-    Route::get('/movies', [MovieController::class, 'show']);
+    Route::get('/movies', [MovieController::class, 'index']);
     Route::get('/movies/{id}', [MovieController::class, 'show']);
 });
